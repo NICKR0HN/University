53d5d2964b1f018d9d773d9f3c41abd75d865fb7
NickRohn <tommaso.giussani01@gmail.com>
2021-03-19 17:12:06 +0100

    added correlation coefficient

--- 03_estensimetro/estensimetro.cxx
+++ 03_estensimetro/estensimetro.cxx
@@ -24,24 +24,36 @@ template <typename T> void Cell(T text){
     cout<< setw(14) << right << text;
 }
 
+//funzioni algebriche
 double Compatibility(double, double, double, double);
 double WeightedMean(vector<double>, vector<double>);
 double MeanSigma(vector<double>);
+double Mean(vector<double>);
+double Summation(vector<double>);
+double GetStdDev(vector<double>, int);
 
 struct sample_struct{
+    // variabili della struttura
     string filename;
     int data_size = 0;
-    vector<double> forces, lengths;
+    vector<double> forces, d_forces, lengths;
     double force_sigma, length_sigma;
     double k_line, q_line, k_line_sig, q_line_sig;
-    double sigma_post, k_line_sig_p, q_line_sig_p;
+    double corr_coeff, sigma_post, k_line_sig_p, q_line_sig_p;
     array<double,2> k_mean;
     array<double,2> k_mean_sigma;
     double comp_a_0, comp_a_1, comp_0_1;
+
+    // costruttore
     sample_struct(string filepath){
         filename = filepath;
         ReadFile();
+        for (double force : forces){
+            double d_force = force - forces[0];
+            d_forces.push_back(d_force);
+        }
         Interpol();
+        CoeffCorr();
         for (int i = 0; i < 2; i++){
             array<double,2> out = KVector(i);
             k_mean[i] = out[0];
@@ -53,6 +65,7 @@ struct sample_struct{
         PrintData();
     }
 
+    //funzioni della struttura
     void ReadFile(){
         ifstream input_file(filename);
         if (!input_file.is_open()){
@@ -74,14 +87,14 @@ struct sample_struct{
         }
         input_file.close();
     }
-
+    
     void Interpol(){
         double xone = 0.0, xtwo = 0.0, yone = 0.0, xy = 0.0;
         for (int i = 0; i < data_size; i++){
-            xone += forces[i];
-            xtwo += pow(forces[i], 2.0);
+            xone += d_forces[i];
+            xtwo += pow(d_forces[i], 2.0);
             yone += lengths[i];
-            xy += forces[i] * lengths[i];
+            xy += d_forces[i] * lengths[i];
         }
         double delta = (data_size * xtwo) - (xone * xone);
         k_line = ((data_size * xy) - (xone * yone)) / delta;
@@ -90,10 +103,22 @@ struct sample_struct{
         q_line_sig = length_sigma * sqrt(xtwo / delta);
         double num = 0.0;
         for (int i = 0; i < data_size; i++)
-            num += pow ((lengths[i] - (k_line * forces[i]) - q_line), 2.0);
+            num += pow ((lengths[i] - (k_line * d_forces[i]) - q_line), 2.0);
         sigma_post = sqrt(num / (data_size - 2.0));
         k_line_sig_p = sigma_post * sqrt(data_size / delta);
         q_line_sig_p = sigma_post * sqrt(xtwo / delta);
+    }
+
+    void CoeffCorr(){
+        vector<double> xys;
+        for (int i = 0; i < data_size; i++)
+            xys.push_back(d_forces[i] * lengths[i]);
+        double xs_mean = Mean(d_forces);
+        double ys_mean = Mean(lengths);
+        double xys_mean = Mean(xys);
+        double var_x = GetStdDev(d_forces, data_size);
+        double var_y = GetStdDev(lengths, data_size);
+        corr_coeff = (xys_mean - xs_mean * ys_mean) / (var_x * var_y);
     }
     
     array<double,2> KVector(int odd){
@@ -104,7 +129,7 @@ struct sample_struct{
         df_sigma = force_sigma * sqrt(2.0);
         for (int i = odd; i < (data_size - 1); i++){
             dx = lengths[i + 1] - lengths[i];
-            df = forces[i + 1] - forces[i];
+            df = d_forces[i + 1] - d_forces[i];
             k = dx / df;
             k_vector.push_back(k);
             k_sigma = abs(k) * sqrt(pow((dx_sigma / dx), 2.0) + pow((df_sigma / df), 2.0));
@@ -127,7 +152,11 @@ struct sample_struct{
         cout<< setw(15) << left << "Interpol.";                     Cell(k_line);       Cell(k_line_sig);       Cell(q_line);   Cell(q_line_sig);   cout<<endl;
         cout<< setw(15) << left << "Post. sigma";                   Cell(k_line);       Cell(k_line_sig_p);     Cell(q_line);   Cell(q_line_sig_p); cout<<endl;
         cout<< setw(15) << left << "Mean k1";                       Cell(k_mean[0]);     Cell(k_mean_sigma[0]);   cout<<endl;
-        cout<< setw(15) << left << "Mean k2";                       Cell(k_mean[1]);     Cell(k_mean_sigma[1]);   cout<<endl;
+        cout<< setw(15) << left << "Mean k2";                       Cell(k_mean[1]);     Cell(k_mean_sigma[1]);   cout<<endl<<endl;
+        cout<< setw(15) << left << "Post. sigma";                   Cell(sigma_post);   cout<<endl;
+        cout<< setprecision(6);
+        cout<< setw(15) << left << "Corr. coeff.";                  Cell(corr_coeff);   cout<<endl;
+        cout<< setprecision(5);
         cout<< setw(15) << left << "Comp. line-k1";                 Cell(comp_a_0);     cout<<endl;
         cout<< setw(15) << left << "Comp. line-k2";                 Cell(comp_a_1);     cout<<endl;
         cout<< setw(15) << left << "Comp. k1-k2";                   Cell(comp_0_1);     cout<<endl;
@@ -221,4 +250,26 @@ double MeanSigma(vector<double> sigmas){
         sig += sigma * sigma;
     double MeanSigma = sqrt(sig) / sigmas.size();
     return MeanSigma;
+}
+
+// media aritmetica
+double Mean(vector<double> data){
+    double sum = 0.0;
+    for (auto c : data)
+        sum += c;
+    double mean = sum / data.size();
+    return mean;
+}
+// sommatoria del quadrato degli scarti
+double Summation(vector<double> data){
+    double summation = 0.0;
+    double m = Mean(data);
+    for (auto c : data)
+        summation += pow((c - m), 2.0);
+    return summation;
+}
+// formula generica della deviazione standard
+double GetStdDev(vector<double> data, int data_len){
+    double std_dev = sqrt(Summation(data) / data_len);
+    return std_dev;
 }
